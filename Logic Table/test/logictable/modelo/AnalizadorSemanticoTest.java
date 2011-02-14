6d12861fd90fe760c8567ff4182501e225d7f625
eduarditolima <eduarditolima@af476c37-49f4-bb60-4b39-f33cf8021a3f>
2011-02-14 10:26:28 +0000

    "Control de errores"

---
+++ Logic Table/test/logictable/modelo/AnalizadorSemanticoTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logictable.modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author EDÚ
+ */
+public class AnalizadorSemanticoTest {
+
+    public AnalizadorSemanticoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of valoresTabla method, of class AnalizadorSemantico.
+     */
+    @Test
+    public void testValoresTabla() {
+        System.out.println("valoresTabla");
+        AnalizadorSemantico instance = null;
+        TablaVerdad expResult = null;
+        //TablaVerdad result = instance.valoresTabla();
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+}
