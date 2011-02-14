b0db136a5d22bff7f1194fb6b2b44239674dd94a
yaneci1611@gmail.com <yaneci1611@gmail.com@af476c37-49f4-bb60-4b39-f33cf8021a3f>
2011-02-14 20:51:03 +0000

    Comentarios Metodos

--- trunk/Logic Table/src/logictable/vista/AcercaDe.java
+++ trunk/Logic Table/src/logictable/vista/AcercaDe.java
@@ -56,16 +56,6 @@ public class AcercaDe extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AcercaDe().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
